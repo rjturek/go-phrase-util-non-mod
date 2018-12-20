bf159317e5a7a8662c0406836f733b38d9c1f09f
Richard Turek <rjt1357@gmail.com>
2018-12-19 23:00:30 -0500

    Update phrasenonmod.go

--- rjtphrasenonmod/phrasenonmod.go
+++ rjtphrasenonmod/phrasenonmod.go
@@ -2,7 +2,7 @@ package rjtphrasenonmod
 
 import "github.com/golang/example/stringutil"
 
-var phrase = "Far away from night-clubs and from saxophones"
+var phrase = "Far away from night-clubs and from saxophones."
 
 func GetPhrase() string {
 	return stringutil.Reverse(stringutil.Reverse(phrase))
